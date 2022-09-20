26ebbf7bc3d979e98e69d1912b22b1b207195d5c
Matt Newsted <mnewsted@illinoislegalaid.org>
2022-09-20 08:41:38 -0500

    QA testing complete

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import convert_path
 
 standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
 def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
     out = {}
     stack = [(convert_path(where), '', package)]
@@ -43,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.EvictATenant',
-      version='0.9.2',
+      version='1.0.0',
       description=('A docassemble extension.'),
-      long_description='# docassemble.EvictATenant\n\nA docassemble extension.\n\n## Author\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
+      long_description='# docassemble.EvictATenant\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Matt Newsted',
       author_email='mnewsted@illinoislegalaid.org',
